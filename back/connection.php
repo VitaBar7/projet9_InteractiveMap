07142f5179bb0ffd15dabdbd9be436627f461c43
VitaBar7 <104008965+VitaBar7@users.noreply.github.com>
2022-12-30 11:21:03 +0100

    style font update, form

--- back/connection.php
+++ back/connection.php
@@ -38,23 +38,25 @@ $_SESSION['authentification'] = "OK";
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Poppins:wght@200;300&display=swap"
-      rel="stylesheet"
-    />
+      rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css" />
   </head>
 
-  <body class="conn">
+  <body>
   <!-- Header added automatically by JS -->
   <header></header>
-    <main>
-        <div id="form_conn" >
-        <form action="connection.php" method="post">
-            <h2>Merci de vous connecter</h2>
-            <input type="email" name="usermail" placeholder="Votre email">
-            <input type="password" name="password" id="pass"placeholder="votre mot de passe">
-            <input type="submit" value="envoyer">
-        </form>
-        </div>
-    </main>
+  <main>
+      <div id="form_conn" >
+      <form action="connection.php" method="post">
+          <h2>Merci de vous connecter</h2>
+          <input type="email" name="usermail" placeholder="Votre email">
+          <input type="password" name="password" id="pass"placeholder="votre mot de passe">
+          <input type="submit" value="envoyer">
+      </form>
+      </div>
+  </main>
+  <footer></footer>
+
   </body>
 </html>
